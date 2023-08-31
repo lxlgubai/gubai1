2022b44b582094658d8beb9f5fa4febfd1b35526
gubai <2120395244@qq.com>
2023-08-31 17:00:46 +0800

    创建

---
+++ service-admin/src/main/java/com/guming/AdminApplication.java
@@ -0,0 +1,17 @@
+package com.guming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Title: AdminApplication
+ * @Package com.guming
+ * @Date 2023/8/31 16:54
+ * @description: 管理员启动类
+ */
+@SpringBootApplication
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
